ca982a40c32c99e866eb4dfa647fbec6870981ed
dZinKii <65257559+dZinKii@users.noreply.github.com>
2020-05-13 00:11:24 +0200

    Add files via upload

---
+++ Soft.bat
@@ -0,0 +1,8 @@
+choco install googlechrome -y
+choco install firefox -y
+choco install vlc -y
+choco install winrar -y
+choco install libreoffice-fresh -y
+choco install teamviewer.host -y
+choco install adobereader -y
+exit
